90855850e3b1d4c314f9c218706cc969bb5cd16c
sabaferdous12 <saba.ferdous.13@ucl.ac.uk>
2022-06-23 14:43:40 +0100

    try to fix LFS...

--- setup.sh
+++ setup.sh
@@ -13,10 +13,4 @@ sudo apt update -y
 
 sudo apt install -y texlive-latex-base texlive-fonts-recommended texlive-fonts-extra texlive-latex-extra
 
-sudo apt-get install software-properties-common
 
-sudo curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | sudo bash*
-
-sudo apt-get install git-lfs
-
-git lfs install
